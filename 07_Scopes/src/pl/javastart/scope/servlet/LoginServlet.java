7551a3dc13ffb3e2c90977b557fbf06804c07978
Łukasz <lzwolan@wp.pl>
2016-10-04 10:20:42 +0200

    Zasięg atrybutów

---
+++ 07_Scopes/src/pl/javastart/scope/servlet/LoginServlet.java
@@ -0,0 +1,50 @@
+package pl.javastart.scope.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String login = request.getParameter("login");
+		if(request.getAttribute("login") == null)
+			request.setAttribute("login", login);
+		
+		response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html");
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+        writer.println("<head><title>Witaj " + request.getAttribute("login") + "</title></head>");
+        writer.println("<body>");
+        writer.println("<h1>Witaj " + request.getAttribute("login") + "</h1>");
+        writer.println("</body>");
+        writer.println("</html>");
+	}
+
+}
